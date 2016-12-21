139943d96f165488635617088ade0665f1f56ae3
willywos <wwilimek@prositions.com>
2016-12-21 16:38:27 -0600

    Derp, wrong object that has the account.

--- lib/acts_as_voter.rb
+++ lib/acts_as_voter.rb
@@ -89,7 +89,7 @@ module ThumbsUp #:nodoc:
         end
         direction = (options[:direction].to_sym == :up)
         # create! does not return the created object
-        v = Vote.new(:vote => direction, :voteable => voteable, :voter => self, :account => voteable.account)
+        v = Vote.new(:vote => direction, :voteable => voteable, :voter => self, :account => self.account)
         v.save!
         v
       end
